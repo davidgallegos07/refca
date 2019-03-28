3d889e0ccfecf90a429e6681a09d17e06945a522
AliciaPeraza <alicia.peraza@uabc.edu.mx>
2019-03-28 09:34:17 -0700

    Update con AcademicBody

--- Models/AcademicBodyViewModels/AcademicBodyViewModel.cs
+++ Models/AcademicBodyViewModels/AcademicBodyViewModel.cs
@@ -6,17 +6,17 @@ namespace refca.Models.AcademicBodyViewModels
 {
     public class AcademicBodyViewModel
     {
-        [Required(ErrorMessage="El nombre del cuerpo académico es requerido")]
-        [StringLength(100, ErrorMessage="Mínimo {2} caracteres, Máximo {1} caracteres", MinimumLength = 2)]
+        [Required(ErrorMessage = "El nombre del cuerpo académico es requerido")]
+        [StringLength(100, ErrorMessage = "Mínimo {2} caracteres, Máximo {1} caracteres", MinimumLength = 2)]
         [Display(Name = "Nombre")]
         public string Name {get; set;}
 
-        [Required(ErrorMessage="El código es requerido")]
+        [Required(ErrorMessage = "El código es requerido")]
         [StringLength(100, ErrorMessage="Mínimo {2} caracteres, Máximo {1} caracteres", MinimumLength = 2)]
         [Display(Name = "Código promep")]
         public string PromepCode {get; set;}
 
-        [Required(ErrorMessage="El grado de consolidación es requerido")]
+        [Required(ErrorMessage = "El grado de consolidación es requerido")]
         [Display(Name = "Grado de consolidación")]
         public byte ConsolidationGradeId {get; set;}
 
